f13542cefb34e22465f613e833cd250f60bf2d9e
hanjo8813 <hanjo8813@gmail.com>
2021-01-20 07:28:01 +0900

    .

--- Graduate Web/projcet/settings.py
+++ Graduate Web/projcet/settings.py
@@ -130,12 +130,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 # static file 경로 설정
-STATIC_URL = '/static/'
-STATIC_DIR = os.path.join(BASE_DIR, 'static')
+
 STATICFILES_DIRS = [
-    STATIC_DIR,
+    os.path.join(BASE_DIR, 'app', 'static')
 ]
-STATIC_ROOT = os.path.join(ROOT_DIR, '.static_root')
+
+STATIC_URL = '/col_static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'col_static')
 
 
 
