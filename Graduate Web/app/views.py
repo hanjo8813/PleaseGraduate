7a93b7ee120bac385d26d1d26174572a6e9a2230
hanjo8813 <hanjo8813@gmail.com>
2021-01-20 04:18:39 +0900

    .

--- Graduate Web/app/views.py
+++ Graduate Web/app/views.py
@@ -588,8 +588,8 @@ def r_en_result(request):
 # --------------------------------------------- (셀레니움 파트) ----------------------------------------------------------------
 
 def get_Driver(url):
-    display = Display(visible=0, size=(1024, 768))
-    display.start()
+    #display = Display(visible=0, size=(1024, 768))
+    #display.start()
     options = webdriver.ChromeOptions()
     # 크롬창을 열지않고 백그라운드로 실행
     # options.add_argument("headless")
