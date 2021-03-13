2853dd9a69ef918d554f1a83f7c37384067148ee
한재원 <hanjo8813@gmail.com>
2021-03-14 03:25:00 +0900

    [Add] 테이블 접기 기능추가

--- Graduate_Web/app/models.py
+++ Graduate_Web/app/models.py
@@ -56,26 +56,6 @@ class TestUserInfo(models.Model):
 
 # ---------------------------------------------------------------------------
 
-'''
-def date_upload_to(instance, filename):
-  # upload_to="%Y/%m/%d" 처럼 날짜로 세분화
-  ymd_path = timezone.now().strftime('%Y/%m/%d') 
-  # 길이 32 인 uuid 값
-  uuid_name = uuid4().hex
-  # 확장자 추출
-  extension = os.path.splitext(filename)[-1].lower()
-  # 결합 후 return
-  return '/'.join([
-    ymd_path,
-    uuid_name + extension,
-  ])
-'''
-
-class MediaUpload(models.Model):
-    #f_name = models.CharField(max_length=100)
-    excel = models.FileField(upload_to='')
-
-
 class DjangoSession(models.Model):
     session_key = models.CharField(primary_key=True, max_length=40)
     session_data = models.TextField()
