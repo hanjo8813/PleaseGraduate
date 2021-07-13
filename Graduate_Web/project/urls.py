c8a3dc73c1b4b236333ac278bff776db64e60859
한재원 <hanjo8813@gmail.com>
2021-07-13 17:58:35 +0900

    chore : projcet -> project 이름 수정

--- Graduate_Web/project/urls.py
+++ Graduate_Web/project/urls.py
@@ -1,4 +1,4 @@
-"""projcet URL Configuration
+"""project URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
