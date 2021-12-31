e64c4eddb6b21d62058837d8ce15b1978cdb6175
한재원 <hanjo8813@gmail.com>
2022-01-01 06:59:29 +0900

    chore: 배포 스크립트 수정 (docker)

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,15 +1,18 @@
 ###!/bin/bash
 
-# GitHub에 등록한 환경변수 받아오기
 DOCKER_USERNAME=$1
-DOCKER_REPOSITORY=$2
-DOCKER_TAG=$3
+DOCKER_PASSWORD=$2
+DOCKER_REPOSITORY=$3
+DOCKER_TAG=$4
 
-echo "================== UPDATE '.env' file =================="
-echo -e "DOCKER_USERNAME=$DOCKER_USERNAME\nDOCKER_REPOSITORY=$DOCKER_REPOSITORY\nDOCKER_TAG=$DOCKER_TAG" > .env
+echo "================== docker login =================="
+docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
 
 echo "================== PULL docker image =================="
 docker pull $DOCKER_USERNAME/$DOCKER_REPOSITORY:$DOCKER_TAG
+
+echo "================== UPDATE '.env' file =================="
+echo -e "DOCKER_USERNAME=$DOCKER_USERNAME\nDOCKER_REPOSITORY=$DOCKER_REPOSITORY\nDOCKER_TAG=$DOCKER_TAG" > .env
 
 echo "================== SERVER DOWN =================="
 docker-compose -p pg_compose down
